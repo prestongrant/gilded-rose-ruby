b9e48a5f1f7b1cd175f7d7d65645d11392dc64e3
Preston Armstrong <pgaxbox@gmail.com>
2024-02-27 21:57:34 -0600

    Refactored into small objects then added into conjure at the end

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,19 +1,16 @@
 module GildedRose
-  def self.new(name, days_remaining, quality)
-    klass_for(name).new(quality, days_remaining)
-  end
-
   class Item
     attr_reader :quality, :days_remaining
 
     def initialize(quality, days_remaining)
-      @quality = quality
-      @days_remaining = days_remaining
+      @quality, @days_remaining = quality, days_remaining
+    end
+
+    def tick
     end
   end
 
   class Normal < Item
-
     def tick
       @days_remaining -= 1
       return if @quality == 0
@@ -33,11 +30,6 @@ module GildedRose
     end
   end
 
-  class Sulfuras < Item
-    def tick
-    end
-  end
-
   class Backstage < Item
     def tick
       @days_remaining -= 1
@@ -48,6 +40,26 @@ module GildedRose
       @quality += 1 if @days_remaining < 10
       @quality += 1 if @days_remaining < 5
     end
+  end
+
+  class Conjured < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality == 0
+
+      @quality -= 2
+      @quality -= 2 if @days_remaining <= 0
+    end
+  end
+
+  DEFAULT_CLASS = Item
+  SPECIALIZED_CLASS = {
+    'normal'                                    => Normal,
+    'Aged Brie'                                 => Brie,
+    'Backstage passes to a TAFKAL80ETC concert' => Backstage,
+    'Conjured Mana Cake'                        => Conjured }
+  def self.new(name, quality, days_remaining)
+    (SPECIALIZED_CLASSES[name] || DEFAULT_CLASS).new(quality, days_remaining)
   end
 
 end
